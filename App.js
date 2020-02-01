9de6924cd961c45d4b7e13c9b706da57cea78df1
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-02-01 13:56:25 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+export default App;
